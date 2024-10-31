e21545cd55de2e852dd3f2a0897578c7dba0e416
moretome1 <99558418+moretome1@users.noreply.github.com>
2024-10-31 14:55:58 +0200

    1.0

--- adrenalin/server.lua
+++ adrenalin/server.lua
@@ -2,9 +2,8 @@ ESX = exports["es_extended"]:getSharedObject()
 
 
 
-ESX.RegisterUsableItem('syringe', function(source)
+ESX.RegisterUsableItem('Adrenaline', function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
-    print("Syringe used by player:", source)  -- Debugging line to check if server event fires
     TriggerClientEvent('esx_syringe:use', source)
-    xPlayer.removeInventoryItem('syringe', 1)      
+    xPlayer.removeInventoryItem('Adrenaline', 1)      
 end)
